f5e1b83e48f6fb8b39c332e58056a28aa014cf35
Roman Klochkov <kalimehtar@mail.ru>
2019-04-29 08:33:43 +0500

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "multicolumn")
 (define deps '("base" "stretchable-snip"))
-;(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+;(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "gui-lib"))
 ;(define scribblings '(("scribblings/multicolumn.scrbl" ())))
 (define pkg-desc "Multicolumn table with data AKA grid")
 (define version "0.1")
